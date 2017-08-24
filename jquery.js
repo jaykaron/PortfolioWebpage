1525d2b9adc71b81554fec03574ec9fd26cef243
jaykaron <jaykaron@gmail.com>
2017-08-24 16:55:47 -0700

    BareBones
    Finished the Bare bones of the layout

--- jquery.js
+++ jquery.js
@@ -24,39 +24,21 @@ $(document).ready(function(){
     });
 
     $(".menuBox").click(function(){scrollToAnchor($(this).attr('linkDes'));});
+    $("#backToTop").click(function(){scrollToTop();});
 });
 
 function smallTop()
 {
   $("#banner").animate({height:"5.5em"})
-  $("#bannerTopLayer").animate({height:"3.75em"})
-  $("#titleDiv").animate({height:"100%", width:"80%", left:"10%", top:"0%"})
-  $("#homeButtonDiv").animate({width:"3.5em"})
-  $("#logo").animate({top:"10%"})
-  $("#menu").animate({top:"3.75em"})
-  $("#about").fadeOut(250);
+  $("#titleDiv").fadeOut(250);
+  $("#backToTop").fadeIn(250);
 }
 
 function bigTop()
 {
   $("#banner").animate({height:topHeight})
-  $("#bannerTopLayer").animate({height: topHeight - menuHeight})
-  $("#titleDiv").animate({height:"20%", width:"15em", top:"70%", left:"0%"})
-  $("#homeButtonDiv").animate({width:"15em"})
-  $("#logo").animate({top:"6%"})
-  $("#menu").animate({top:topHeight - menuHeight})
-  $("#about").delay(200).fadeIn();
-}
-
-function bigTopOld()
-{
-  $("#banner").animate({height:"20em"})
-  $("#bannerTopLayer").animate({height:"18em"})
-  $("#titleDiv").animate({height:"20%", width:"15em", top:"70%", left:"0%"})
-  $("#homeButtonDiv").animate({width:"15em"})
-  $("#logo").animate({top:"6%"})
-  $("#menu").animate({height:"1.75em", top:"18em"})
-  $("#about").delay(200).fadeIn();
+  $("#titleDiv").delay(200).fadeIn();
+  $("#backToTop").fadeOut(250);
 }
 
 function scrollToAnchor(anchorName)
@@ -64,4 +46,9 @@ function scrollToAnchor(anchorName)
   var idString= "h2[name='"+anchorName+"']"
   var anchorObj = $(idString);
   $("html,body").animate({scrollTop: anchorObj.offset().top-100},'slow');
+}
+
+function scrollToTop()
+{
+  $("html,body").animate({scrollTop: 0},'slow');
 }
